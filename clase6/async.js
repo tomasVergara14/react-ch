9a75d104c634bb71b26542879ece961da39499c6
tomasVergara14 <tomasvergara272@gmail.com>
2021-07-19 21:15:15 -0300

    digital components2

--- clase6/async.js
+++ clase6/async.js
@@ -1,26 +1,24 @@
-const loadData = async ()=>{
+const prueba = (()=>{
 
     try{
-            
-        const url = 'http://jsonplaceholder.typicode.com/todos/1'
-
-        const res = await fetch(url)
-
-        const data = await res.json()
-
-        return data
+                
+        console.log('a ver')
+        const time =  setTimeout(()=>{
+                console.log('ya era hora, va tercero')
+                setTimeout(()=>{
+                    console.log('y este va cuarto')
+                },2000)
+            },1000)
+        //console.log(time)
+        console.log('esto es segundo')
     }
-    catch{
-        err=>{
-            console.log(err)
-        }
+    catch(err){
+        console.log(err)
     }
-    
-}
-
-(async ()=>{
-    const resultado = await loadData()
-    console.log(resultado)
 })()
 
-
+const print =  (async ()=>{
+    const data = await prueba
+    console.log(data)
+    console.log('prnt')
+})()
